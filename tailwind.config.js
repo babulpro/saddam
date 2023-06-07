4eb17228b0bff9714e74d2f6598ace697546c440
babulpro <124435665+babulpro@users.noreply.github.com>
2023-06-07 10:11:22 +0600

    file add
    change

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'media', 
+  content: ["index.html","style.html"],
+ 
+  theme: {
+    extend: {
+
+      // height: {
+      //   '128': '32rem',
+      //  }
+    },
+  },
+  plugins: [],
+}
